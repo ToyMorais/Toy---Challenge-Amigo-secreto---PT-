ec2afe3ac9627b568769ee3dd2d5ab287b211c02
ToyMorais <ajcmoraisfilho@gmail.com>
2025-02-23 20:26:50 -0300

    quarta entrada modifica o leitor de tela e o criteriza

--- app.js
+++ app.js
@@ -9,27 +9,34 @@ let resultadoIndex = 0;
 function exibirTextoNaTela(tag, title) {
     let campo = document.querySelector(tag);
     campo.innerHTML = title;
-    // ######### executa script no HTML para ler a tela ###########
-    responsiveVoice.speak(title, "Brazilian Portuguese Female", {rate: 1.2});
-    // ######### fim do comando para ler a tela ###########   
+    // ######### executa script para ler a tela ###########
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(title);
+        utterance.lang = 'pt-BR';
+        utterance.rate = 1;
+        if (tag !== "ul") {
+            window.speechSynthesis.speak(utterance);
+        }
+    } else {
+        console.log("Web Speech API não suportado neste navegador.");
+    }
+    // ######### fim do script ###########
+    
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela("h1", "Amigo Secreto");
-    exibirTextoNaTela("h2", `Digite o nome dos seus amigos e clique em sortear.`);
-    exibirTextoNaTela("ul", `Amigos incluídos: ${listaAmigos}`);
+    exibirTextoNaTela("h2", `Digite o nome dos seus amigos e clique em sortear.`);    
 }
 exibirMensagemInicial();
 
 function adicionarAmigo() {
     let nome = document.querySelector("input").value;
-    console.log(nome);
        if (listaAmigos.includes(nome)) { 
         exibirTextoNaTela("h2", `Você já incluiu este amigo na lista!`);
     } else { 
         listaAmigos.push(nome);    
         exibirTextoNaTela("h2", "Incluia o nome de outro amigo!");
-        exibirTextoNaTela("ul", listaAmigos)
-        console.log(listaAmigos);
+        exibirTextoNaTela("ul", listaAmigos.join(", "));
         limparCampo();
     }
 }
