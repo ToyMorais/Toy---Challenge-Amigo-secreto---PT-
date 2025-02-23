d6b13a9adb4b587cb89a08f845402db3a37134c1
ToyMorais <ajcmoraisfilho@gmail.com>
2025-02-23 19:09:26 -0300

    segunda entrega com a função adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Jogo Amigo Secreto
+
+let listaAmigos = [];
+let listaAmigosSorteados = [];
+let resultado = "";
+let resultadoIndex = 0;
+
+function exibirTextoNaTela(tag, title) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = title;
+}
+function exibirMensagemInicial() {
+    exibirTextoNaTela("h1", "Amigo Secreto");
+    exibirTextoNaTela("h2", `Digite o nome dos seus amigos e clique em sortear.`);
+    exibirTextoNaTela("ul", `Amigos incluídos: ${listaAmigos}`);
+}
+exibirMensagemInicial();
+
+function adicionarAmigo() {
+    let nome = document.querySelector("input").value;
+    console.log(nome);
+       if (listaAmigos.includes(nome)) { 
+        exibirTextoNaTela("h2", `Você já incluiu este amigo na lista!`);
+    } else { 
+        listaAmigos.push(nome);    
+        exibirTextoNaTela("h2", "Incluia o nome de outro amigo!");
+        exibirTextoNaTela("ul", listaAmigos)
+        console.log(listaAmigos);
+        limparCampo();
+    }
+}
