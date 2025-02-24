9bde04fcb3702ac7d96e76fcab8c2df574698f28
ToyMorais <ajcmoraisfilho@gmail.com>
2025-02-23 21:25:44 -0300

    quinta entrada incluidas funções sortear amigo e limpar campo

--- app.js
+++ app.js
@@ -20,23 +20,48 @@ function exibirTextoNaTela(tag, title) {
     } else {
         console.log("Web Speech API não suportado neste navegador.");
     }
-    // ######### fim do script ###########
-    
+    // ######### fim do script ########### 
+}
+function limparCampo() {
+    nome = document.querySelector("input");
+    nome.value = "";
+    nome.focus();
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela("h1", "Amigo Secreto");
-    exibirTextoNaTela("h2", `Digite o nome dos seus amigos e clique em sortear.`);    
+    exibirTextoNaTela("h2", `Digite o nome dos seus amigos.`);    
 }
 exibirMensagemInicial();
 
 function adicionarAmigo() {
     let nome = document.querySelector("input").value;
-       if (listaAmigos.includes(nome)) { 
+    if (listaAmigos.includes(nome)) { 
         exibirTextoNaTela("h2", `Você já incluiu este amigo na lista!`);
     } else { 
         listaAmigos.push(nome);    
-        exibirTextoNaTela("h2", "Incluia o nome de outro amigo!");
-        exibirTextoNaTela("ul", listaAmigos.join(", "));
-        limparCampo();
+        exibirTextoNaTela("h2", "Incluia outro amigo!");
+        exibirTextoNaTela("ul", listaAmigos.join(", "));  
+    }
+limparCampo();  
+
+}
+function sortearAmigo() {
+    let quantidadeDeAmigos = listaAmigos.length;
+    if (quantidadeDeAmigos < 2) {
+        exibirTextoNaTela("h2", `Você precisa incluir pelo menos 2 amigos!`);
+    } else {
+        let amigoSorteado = listaAmigos[Math.floor(Math.random() * quantidadeDeAmigos)];
+        if (listaAmigosSorteados.includes(amigoSorteado)) {
+            sortearAmigo();
+        } else {
+            listaAmigosSorteados.push(amigoSorteado);          
+            resultado = `O seu amigo secreto é ${amigoSorteado}`;
+            resultadoIndex++;
+            if (resultadoIndex < quantidadeDeAmigos) {               
+                sortearAmigo();
+            } else {
+                exibirTextoNaTela("h2", resultado);
+            }
+        }
     }
 }
